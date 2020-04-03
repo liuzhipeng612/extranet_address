f418ec00a6ba7f29cab771cd78e5ac43ca8fa629
Daodao <57635630+liuzhipeng612@users.noreply.github.com>
2020-04-04 01:35:55 +0800

    新建项目

---
+++ ip138.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+
+import re
+
+import requests
+
+html_text = requests.get("https://2020.ip138.com/").text
+# 正则匹配方式1
+
+ip_text = re.search(u'<span class="cf-footer-item"><span>Your IP</span>: (.*?)</span>', html_text)
+
+print(ip_text.group(1))
+
+# 正则匹配方式2
+ip_text = re.findall(u'<span class="cf-footer-item"><span>Your IP</span>: (.*?)</span>', html_text)
+print(ip_text[0])
